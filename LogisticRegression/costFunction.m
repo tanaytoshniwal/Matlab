d858f3d0632780d91c37f4c51dd9b22082bd4f7a
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2018-01-13 16:42:14 +0530

    logistic regression

--- LogisticRegression/costFunction.m
+++ LogisticRegression/costFunction.m
@@ -6,7 +6,7 @@ gradient = zeros(2,1);
 gradient(1) = 2*(theta(1)-5);
 gradient(2) = 2*(theta(2)-5);
 
-% options = optimset('GradObj', 'on', 'MaxIter', '100');
+% options = optimset('GradObj', 'on', 'MaxIter', 100);
 % initialTheta = zeros(2,1);
 % [optTheta, functionVal, exitFlag] = fminunc(@costFunction, initialTheta, options);
 
